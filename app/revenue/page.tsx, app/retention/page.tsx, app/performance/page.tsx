b6d55f1cafd87368595681fa1725ee3cb7f676b3
growth easy analytics <seanwoodward003@gmail.com>
2025-12-10 18:48:32 +0000

    Create page.tsx

---
+++ app/revenue/page.tsx, app/retention/page.tsx, app/performance/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="text-center py-20">
+      <h1 className="text-6xl font-black text-neon animate-glitch">[PAGE NAME] COMING SOON</h1>
+    </div>
+  );
+}
