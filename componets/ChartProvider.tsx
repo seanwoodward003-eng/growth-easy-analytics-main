9dc43424ce6cdac187cdaca90c7c30896e312791
growth easy analytics <seanwoodward003@gmail.com>
2025-12-10 17:42:45 +0000

    Create ChartProvider.tsx

---
+++ componets/ChartProvider.tsx
@@ -0,0 +1,11 @@
+// components/ChartProvider.tsx
+'use client';
+
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, BarElement, ArcElement, Title, Tooltip, Legend, Filler } from 'chart.js';
+import { ReactNode } from 'react';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement, ArcElement, Title, Tooltip, Legend, Filler);
+
+export default function ChartProvider({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
