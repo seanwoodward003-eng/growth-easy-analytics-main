ee4ab5bcb7982097a582d6ba799f5866f7df3414
growth easy analytics <seanwoodward003@gmail.com>
2025-12-12 20:56:18 +0000

    Delete page.tsx

--- app/revenue/page.tsx, app/retention/page.tsx, app/performance/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="text-center py-20">
-      <h1 className="text-6xl font-black text-neon animate-glitch">[PAGE NAME] COMING SOON</h1>
-    </div>
-  );
-}
