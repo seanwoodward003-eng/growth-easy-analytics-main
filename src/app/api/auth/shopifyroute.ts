abdb271532797346c306e5743c22a09815ef19ba
growth easy analytics <seanwoodward003@gmail.com>
2025-12-13 22:35:13 +0000

    Create shopifyroute.ts

---
+++ src/app/api/auth/shopifyroute.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.redirect("https://growth-easy-analytics-2.onrender.com/api/auth/shopify/start");
+}
