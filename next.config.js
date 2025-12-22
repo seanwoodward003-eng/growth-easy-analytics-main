7d639fc7a708bc07ddef3e57481d3a6785dd9bb5
growth easy analytics <seanwoodward003@gmail.com>
2025-12-22 20:47:48 +0000

    Remove static export option from next.config.js
    Removed the static export option from the Next.js configuration.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // <-- this line enables static export
+  
   // Optional: for clean URLs ( /dashboard instead of /dashboard.html )
   trailingSlash: true,
 };
