41709910907f9699e058093fd0e1bba3437cbf12
growth easy analytics <seanwoodward003@gmail.com>
2025-12-23 22:34:43 +0000

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,11 +6,7 @@ import { useRouter } from 'next/navigation';
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
-  /* useEffect(() => {
-    if (!document.cookie.includes('access_token')) {
-      router.push('/');
-    }
-  }, [router]); *\
+  
 
   return <>{children}</>;
 } 
