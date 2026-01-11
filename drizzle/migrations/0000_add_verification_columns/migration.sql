6a5c4177350619f8d907d20476ebdf6358c1d169
growth easy analytics <seanwoodward003@gmail.com>
2026-01-11 21:49:37 +0000

    Update migration.sql

--- drizzle/migrations/0000_add_verification_columns/migration.sql
+++ drizzle/migrations/0000_add_verification_columns/migration.sql
@@ -1,3 +1,3 @@
 -- drizzle/migrations/0000_add_verification_columns/migration.sql
 ALTER TABLE users ADD COLUMN verification_token TEXT;
-ALTER TABLE users ADD COLUMN verification_token_expires TEXT;
+ALTER TABLE users ADD COLUMN verification_token_expires TEXT; 
