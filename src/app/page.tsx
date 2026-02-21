f71e630fbc7b145558caa33ed7e551afadc2eeee
growth easy analytics <seanwoodward003@gmail.com>
2026-02-21 22:41:57 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-// app/page.tsx
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = 'force-no-store';
@@ -261,4 +260,4 @@ export default function LandingPage() {
       </footer>
     </main>
   );
-} 
+}
